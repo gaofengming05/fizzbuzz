4f8072b1bf37b1fa085afe11c2993692dc29c438
gaofengming05 <3312798348@qq.com>
2025-07-01 10:45:18 +0800

    Update fizzbuzz.cpp
    finish test01

--- fizzbuzz.cpp
+++ fizzbuzz.cpp
@@ -12,10 +12,16 @@ bool check(myOut out, myOut Out) {
 void analysis(myIn in, myOut& out) {
 	if (in % 3 == 0 && in % 5 == 0) { out = "FizzBuzz"; }
 	if (in % 3 == 0) { out = "Fizz"; }
-	if (in % 5 == 0) { out == "Buzz"; }
+	if (in % 5 == 0) { out = "Buzz"; }
 	else { out = to_string(in); }
 }
-
+bool test01() {
+	myIn in = 4;
+	myOut out;
+	analysis(in, out);
+	myOut Out = "1";
+	check(out, Out);
+}
 
 
 
